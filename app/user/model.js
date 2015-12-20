bc2492595b04bfde7f80dbaf1a5cedf0f2f1b3a5
Chris Roerig <chris.roerig@thefreeosk.com>
2015-12-20 12:34:15 -0600

    Added a profile page

--- app/user/model.js
+++ app/user/model.js
@@ -1,8 +1,29 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
     username: DS.attr('string'),
-    numberOfChirps: DS.attr('number'),
-    numberOfFollowing: DS.attr('number'),
-    numberOfFollowers: DS.attr('number')
+    aboutMe: DS.attr('string'),
+    joinedAt: DS.attr('date'),
+    chirps: DS.hasMany('chirp', {
+        async: true
+    }),
+    followers: DS.hasMany('user', {
+        inverse: 'followees'
+    }),
+    followees: DS.hasMany('user', {
+        inverse: 'followers'
+    }),
+    numberOfChirps: Ember.computed('chirps',
+        function() {
+            return this.get('chirps').get('length');
+    }),
+    numberOfFollowing: Ember.computed('followees',
+        function(){
+            return this.get('followees').get('length');
+    }),
+    numberOfFollowers: Ember.computed('followers',
+        function(){
+            return this.get('followers').get('length');
+   })
 });
